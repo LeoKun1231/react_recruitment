4e4c5c090309f1ccc9c365e1c0e0e25fcdd35b28
Leo <1024983409@qq.com>
2023-05-22 00:53:00 +0800

    fix: 解决一些Bug

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 /*
  * @Author: hqk
  * @Date: 2023-02-16 11:20:26
- * @LastEditors: hqk
- * @LastEditTime: 2023-05-21 23:21:27
+ * @LastEditors: leo
+ * @LastEditTime: 2023-05-22 00:50:05
  * @Description:
  */
 import React from 'react'
@@ -10,12 +10,12 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { DndProvider } from 'react-dnd'
 import { HTML5Backend } from 'react-dnd-html5-backend'
+import { Provider } from 'react-redux'
 import App from '@/App'
 
 import '@unocss/reset/normalize.css'
 import '@/assets/css/index.less'
 
-import { Provider } from 'react-redux'
 import 'uno.css'
 import store from './store'
 
